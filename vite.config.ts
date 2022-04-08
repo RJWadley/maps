fb4d537f912653dd2e0d1fabccc0a4291e62503f
Robbie <robbie@reformcollective.com>
2022-04-07 23:57:46 -0600

    add config option

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,14 @@ import { VitePWA } from "vite-plugin-pwa";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    react(),
     VitePWA({
+      strategies: "injectManifest",
       srcDir: "",
       filename: "sw.ts",
       devOptions: {
         enabled: true,
       },
     }),
-    react(),
   ],
 });
