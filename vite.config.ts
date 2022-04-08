0ea79dd26adef8d692d873f39e8a12c543bb613b
Robbie <robbie@reformcollective.com>
2022-04-08 00:14:16 -0600

    Revert "add config option"
    This reverts commit fb4d537f912653dd2e0d1fabccc0a4291e62503f.

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,13 @@ import { VitePWA } from "vite-plugin-pwa";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
     VitePWA({
-      strategies: "injectManifest",
       srcDir: "",
       filename: "sw.ts",
       devOptions: {
         enabled: true,
       },
     }),
+    react(),
   ],
 });
