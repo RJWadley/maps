088b301c19de3a63aa1b6f7d076b1fb80230e8f8
Robbie <robbie@reformcollective.com>
2022-04-08 21:06:24 -0600

    short circuit if out of bounds

--- serviceworker.js
+++ serviceworker.js
@@ -81,21 +81,12 @@ handleRequest = async (event) => {
         });
       })
     );
+  } else {
+    //return 404
+    event.respondWith(
+      new Response("", {
+        status: 404,
+      })
+    );
   }
 };
-
-// self.addEventListener("fetch", function (event) {
-//   event.respondWith(
-//     caches.open("mysite-dynamic").then(function (cache) {
-//       return cache.match(event.request).then(function (response) {
-//         return (
-//           response ||
-//           fetch(event.request).then(function (response) {
-//             cache.put(event.request, response.clone());
-//             return response;
-//           })
-//         );
-//       });
-//     })
-//   );
-// });
